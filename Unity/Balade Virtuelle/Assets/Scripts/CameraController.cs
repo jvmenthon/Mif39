5d89d35c5c09d890bd9a0b3ff43db7e0a20fb93f
jayce89 <jacen.shan@gmail.com>
2015-04-23 14:50:56 +0200

    Ajout projet Unity soucoupe

---
+++ Unity/Balade Virtuelle/Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject player;
+	public Camera camera;
+	private Vector3 offset;
+	private float turnSpeed = 5.0f;
+	private int zoom = 5;
+	private int normal = 60;
+	private int cpt = 0;
+	private float smooth = 5.0f;
+	private bool isZoomed = false;
+	private bool isDezoomed = false;
+
+	
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - player.transform.position;
+	}
+
+	void Update(){
+		const int orthographicSizeMin = 1; const int orthographicSizeMax = 6;
+
+		if(Input.GetAxis("Mouse ScrollWheel")>0){ isZoomed = true; isDezoomed = false;
+			Camera.main.orthographicSize++;}
+		else if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+			isDezoomed = true;
+			isZoomed = false;
+			Camera.main.orthographicSize--;
+		} 
+		else {
+			Camera.main.orthographicSize = Mathf.Clamp (Camera.main.orthographicSize, orthographicSizeMin, orthographicSizeMax);
+		}
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+		transform.position = player.transform.position + offset ;
+	}
+
+	void FixedUpdate() {
+
+		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+		transform.position = player.transform.position + offset; 
+		transform.LookAt(player.transform.position);
+	}
+		         
+}
---
+++ Unity/Balade Virtuelle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	LineRenderer line;
+	private float defaultSpeed = 25.0f;
+	public float speed;
+	private float lift = 10.0f;
+	public float force;
+
+	void Start()
+	{
+		line = gameObject.GetComponent<LineRenderer> ();
+		line.enabled = false;
+	}
+
+
+	void Update()
+	{
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			StopCoroutine("FireLaser");
+			StartCoroutine("FireLaser");
+		}
+	}
+
+	void FixedUpdate()
+	{
+
+		if (Input.GetKey (KeyCode.Z))
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + speed*Time.deltaTime);
+		
+		if(Input.GetKey (KeyCode.S))
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - speed*Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.D))
+			transform.position = new Vector3 (transform.position.x + speed*Time.deltaTime, transform.position.y, transform.position.z);
+		
+		if(Input.GetKey (KeyCode.Q))
+			transform.position = new Vector3 (transform.position.x - speed*Time.deltaTime, transform.position.y, transform.position.z);
+
+		if (Input.GetKey (KeyCode.A))
+			transform.position = new Vector3 (transform.position.x, transform.position.y + lift*Time.deltaTime, transform.position.z);
+
+		if(Input.GetKey (KeyCode.E))
+			transform.position = new Vector3 (transform.position.x, transform.position.y - lift*Time.deltaTime, transform.position.z);
+
+		if (Input.GetKey (KeyCode.LeftShift))
+			speed = defaultSpeed * 2;
+	}
+
+	IEnumerator FireLaser()
+	{
+		line.enabled = true;
+
+		while (Input.GetButton("Fire1")) 
+		{
+			//Ray ray = new Ray(transform.position,transform.forward);
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			line.SetPosition(0,transform.position);
+
+			if(Physics.Raycast(ray,out hit, 500))
+			{
+			   line.SetPosition(1,hit.point);
+				hit.rigidbody.AddExplosionForce(force,hit.point,5,0,ForceMode.Impulse);
+			}
+				
+			else
+			   line.SetPosition(1,ray.GetPoint(500));
+
+			yield return null;
+		}
+		line.enabled = false;
+	}
+}
